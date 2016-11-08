7d384bb655c339dc51a60bf4891b24cab5d2befd
Jake Pyne <jakepyne@gmail.com>
2016-11-08 12:41:20 +0100

    Sort quickstarts

--- src/components/platform-list.jsx
+++ src/components/platform-list.jsx
@@ -2,11 +2,35 @@ import React from 'react';
 import Platform from './platform';
 
 class PlatformList extends React.Component {
-  
+
+  constructor() {
+    super();
+    this.comparator = this.comparator.bind(this);
+  }
+
+  comparator(a, b) {
+    // show third party quickstarts last
+    const thirdParty = [
+      this.props.quickstart.platforms[a].third_party,
+      this.props.quickstart.platforms[b].third_party,
+    ];
+
+    if (thirdParty[0] !== thirdParty[1]) {
+      return thirdParty[0] && !thirdParty[1] ? 1 : -1;
+    }
+
+    // sort alphabetically
+    const titles = [
+      this.props.quickstart.platforms[a].title.toLowerCase(),
+      this.props.quickstart.platforms[b].title.toLowerCase(),
+    ];
+
+    return titles[0] < titles[1] ? -1 : titles[0] > titles[1] ? 1 : 0;
+  }
+
   render() {
     let {quickstart, customNavigationAction} = this.props;
-    
-    let items = Object.keys(quickstart.platforms).map((name, i) => (
+    let items = Object.keys(quickstart.platforms).sort(this.comparator).map((name, i) => (
       <Platform
         key={quickstart.name + i}
         delay={20 * i}
